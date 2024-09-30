772854c6aae649f781a785dd709a371c2a92b320
Mantas Trojanovskis <mantastrojak@gmail.com>
2024-10-01 02:38:56 +0300

    Updated gameInfo.html scrip.js style.css
    Added gameInfo Content generation
    Stylized gameInfo Content
    Added Additional tab functionality for platform requirements of games

--- javascript/script.js
+++ javascript/script.js
@@ -108,7 +108,7 @@ const corsProxyCategories = `https://api.allorigins.win/get?url=${encodeURICompo
       
        
        const link = document.createElement('a');
-        link.href = `gameInfo.html?appid=${item.id}`;
+        link.href = `html/gameInfo.html?appid=${item.id}`;
         link.target = '_self';
 
        const img = document.createElement('img');
@@ -133,6 +133,7 @@ const corsProxyCategories = `https://api.allorigins.win/get?url=${encodeURICompo
         
     }
     async function PopulateNewReleases(parsedData) {
+        currentPath = window.location.pathname.split('/').pop();
         const newReleases = parsedData.new_releases.items;
         const container = document.getElementById('new-releases');
         var SteamDeckBool = Boolean(false);
@@ -157,7 +158,16 @@ const corsProxyCategories = `https://api.allorigins.win/get?url=${encodeURICompo
        itemDiv.setAttribute('class', 'new-releases-item');
                 // Create a new link element
        const link = document.createElement('a');
-        link.href = `game.html?appid=${item.id}`;
+       if(currentPath == 'index.html')
+       {
+        link.href = `html/gameInfo.html?appid=${item.id}`;
+        
+       }
+       else
+       {
+        link.href = `gameInfo.html?appid=${item.id}`;
+       }
+        
         link.target = '_self';
                 // Create a new image element
        const img = document.createElement('img');
@@ -206,7 +216,8 @@ const corsProxyCategories = `https://api.allorigins.win/get?url=${encodeURICompo
         }
 
         const data = await response.json();
-        console.log(data);
+        const parsedData = JSON.parse(data.contents);
+        PopulateGameInfo(parsedData)
     }
     catch (error)
     {
@@ -216,13 +227,214 @@ const corsProxyCategories = `https://api.allorigins.win/get?url=${encodeURICompo
     async function PopulateGameInfo(parsedData)
     {
         const container = document.getElementById('game-info');
-        if(container == null)
+        if(parsedData == null)
         {
             return;
         }
-        const gameInfo = parsedData[Object.keys(parsedData)[0]].data;
+        const appid = window.location.search.split('=').pop();
+
+        
+        const gameInfo = parsedData[appid].data;
         console.log(gameInfo);
+        var IsWindows = Boolean(false);
+        var IsMac = Boolean(false);
+        var IsLinux = Boolean(false);
+
+         
+
+        const name = document.createElement('h1');
+        name.innerText = gameInfo.name;
+        container.appendChild(name);
+
+        const img = document.createElement('img');
+        img.src = gameInfo.header_image;
+        img.setAttribute('id', 'game-image');
+        container.appendChild(img);
+        
+
+
+         const detailed_description = document.createElement('p'); 
+         detailed_description.setAttribute('id', 'detailed_description');
+         detailed_description.innerHTML = gameInfo.detailed_description;
+
+            container.appendChild(detailed_description);
+
+        
+        const supported_languages = document.createElement('p');
+        supported_languages.setAttribute('id', 'supported_languages');
+        supported_languages.innerHTML = gameInfo.supported_languages;
+        container.appendChild(supported_languages);
+
+        const requirementsPlatform = document.getElementById('requirements');
+        const requirements_tabs = document.createElement('div');
+        requirements_tabs.setAttribute('class', 'requirements_tabs');
+        
+            if(gameInfo.platforms.windows == true)
+            {
+                IsWindows = true;
+                const platform = document.createElement('div');
+                platform.setAttribute('class', 'platform_tab')
+                platform.setAttribute('id', 'platform-windows');
+                platform.innerText = 'Windows';
+                requirements_tabs.appendChild(platform);
+            }
+            if(gameInfo.platforms.mac == true)
+            {
+                IsMac = true;
+                const platform = document.createElement('div');
+                platform.setAttribute('class', 'platform_tab')
+                platform.setAttribute('id', 'platform-mac');
+                platform.innerText = 'Mac';
+                requirements_tabs.appendChild(platform);
+            }
+            if(gameInfo.platforms.linux == true)
+            {
+                IsLinux = true;
+                const platform = document.createElement('div');
+                platform.setAttribute('class', 'platform_tab')
+                platform.setAttribute('id', 'platform-linux');
+                platform.innerText = 'Linux';
+                requirements_tabs.appendChild(platform);
+            }
+        requirementsPlatform.appendChild(requirements_tabs);
+    
+        const requirements_content = document.createElement('div');
+        requirements_content.setAttribute('class', 'requirements_content');
+        if(IsWindows == true)
+        {
+            const requirementsDiv = document.createElement('div');
+            requirementsDiv.setAttribute('class', 'platform_requirements');
+            requirementsDiv.setAttribute('id', 'requirements-windows');
+            requirementsDiv.innerHTML =`
+             <div id="minimum-requirements">${gameInfo.pc_requirements.minimum}</div>
+             <div id="recommended-requirements">${gameInfo.pc_requirements.recommended}</div>
+            `;
+            requirements_content.appendChild(requirementsDiv);
+
+
+        }
+        if(IsMac == true)
+        {
+            const requirementsDiv = document.createElement('div');
+            requirementsDiv.setAttribute('class', 'platform_requirements');
+            requirementsDiv.setAttribute('id', 'requirements-mac');
+            requirementsDiv.innerHTML =`
+             <div id="minimum-requirements">${gameInfo.mac_requirements.minimum}</div>
+             <div id="recommended-requirements">${gameInfo.mac_requirements.recommended}</div>
+            `;
+            requirements_content.appendChild(requirementsDiv);
+        }
+        if(IsLinux == true)
+        {
+            const requirementsDiv = document.createElement('div');
+            requirementsDiv.setAttribute('class', 'platform_requirements');
+            requirementsDiv.setAttribute('id', 'requirements-linux');
+            requirementsDiv.innerHTML =`
+             <div id="minimum-requirements">${gameInfo.linux_requirements.minimum}</div>
+             <div id="recommended-requirements">${gameInfo.linux_requirements.recommended}</div>
+            `;
+            requirements_content.appendChild(requirementsDiv);
+            
+        }
+
+        requirementsPlatform.appendChild(requirements_content);
+        // add developers and publishers section
+        
+        const devPubSection = document.createElement('div');
+        devPubSection.setAttribute('id', 'dev-pub-section');
+        // add label for developers
+        const developerLabel = document.createElement('h2');
+        developerLabel.innerText = 'Developers:';
+        devPubSection.appendChild(developerLabel);
+        // add developers list
+        const developerList = document.createElement('ul');
+        developerList.setAttribute('id', 'developer-list');
+        gameInfo.developers.forEach(item => {
+            const developer = document.createElement('li');
+            developer.setAttribute('id', 'developer');
+            developer.innerText = item;
+            developerList.appendChild(developer);
+        });
+        devPubSection.appendChild(developerList);
+
+
+        // add label for publishers
+        const publisherLabel = document.createElement('h2');
+        publisherLabel.innerText = 'Publishers:';
+        devPubSection.appendChild(publisherLabel);
+        // add publishers list
+        const publisherList = document.createElement('ul');
+        publisherList.setAttribute('id', 'publisher-list');
+        gameInfo.publishers.forEach(item => {
+            const publisher = document.createElement('li');
+            publisher.setAttribute('id', 'publisher');
+            publisher.innerText = item;
+            publisherList.appendChild(publisher);
+        });
+        devPubSection.appendChild(publisherList);
+
+
+        container.appendChild(devPubSection);
+
+
+
+
+
+
+        // Create Categories and Genres section
+        const catGenSection = document.createElement('div');
+        catGenSection.setAttribute('id', 'cat-gen-section');
+
+        // add label for categories
+        const categoryLabel = document.createElement('h2');
+        categoryLabel.innerText = 'Categories';
+        catGenSection.appendChild(categoryLabel);
+
+        // add categories list
+        const categoryList = document.createElement('ul');
+        categoryList.setAttribute('id', 'category-list');
+        gameInfo.categories.forEach(item => {
+            const category = document.createElement('li');
+            category.setAttribute('id', 'category');
+            category.innerText = item.description;
+            categoryList.appendChild(category);
+        });
+        catGenSection.appendChild(categoryList);
+
+        // add label for genres
+        const genreLabel = document.createElement('h2');
+        genreLabel.innerText = 'Genres';
+        catGenSection.appendChild(genreLabel);  
+
+        // add genres list
+        const genreList = document.createElement('ul');
+        genreList.setAttribute('id', 'genre-list');
+        gameInfo.genres.forEach(item => {
+            const genre = document.createElement('li');
+            genre.setAttribute('id', 'genre');
+            genre.innerText = item.description;
+            genreList.appendChild(genre);
+        });
+        catGenSection.appendChild(genreList);
+
+        container.appendChild(catGenSection);
+
+        document.querySelectorAll('.platform_tab').forEach(tab => {
+            tab.addEventListener('click', function() {
+              // Remove active class from all tabs and requirements
+              document.querySelectorAll('.platform_tab').forEach(t => t.classList.remove('active'));
+              document.querySelectorAll('.platform_requirements').forEach(req => req.classList.remove('active'));
+          
+              // Add active class to the clicked tab and corresponding requirements
+              this.classList.add('active');
+              const platformId = this.getAttribute('id').replace('platform-', 'requirements-');
+              document.getElementById(platformId).classList.add('active');
+            });
+          });
+          
     }
+   
+      
 
   
    
